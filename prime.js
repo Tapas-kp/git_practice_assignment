e075c1d1ae602029951f40b81c7637881ec4f56f
Tapas-kp <palaitapaskumar8@gmail.com>
2023-10-12 11:33:29 +0530

    check my code

---
+++ prime.js
@@ -0,0 +1,25 @@
+function prime_notprime(num)
+{
+    let count=0
+    for(let i=1;i<=num;i++)
+    if(i%num==0)
+    {
+        count+=1
+    }
+    if(count==2)
+    {
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+a=prime_notprime(17)
+if(a==true)
+{
+    console.log("prime number")
+}
+else{
+    console.log("not prime number")
+}
